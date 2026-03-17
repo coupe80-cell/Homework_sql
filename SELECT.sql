43785fc8856cd6c0a1e5ef3afa54a2f8e7fefe59
Valeriy <coupe80@gmail.com>
2026-03-17 07:29:22 +0300

    Add files via upload

---
+++ SELECT.sql
@@ -0,0 +1,74 @@
+--Задание 2
+
+SELECT name AS трек, length AS продолжительность
+FROM tracks
+ORDER BY length DESC
+LIMIT 1;
+
+SELECT name AS трек
+FROM tracks
+WHERE length >= 210;
+
+
+SELECT name AS сборник
+FROM collections
+WHERE year BETWEEN 2018 AND 2020;
+
+
+SELECT name AS трек
+FROM tracks
+WHERE length >= 210;
+
+
+SELECT name AS сборник
+FROM collections
+WHERE year BETWEEN 2018 AND 2020;
+
+
+SELECT name AS исполнитель
+FROM artists
+where name not like '% %';
+
+
+SELECT name AS трек
+FROM tracks
+WHERE LOWER(name) LIKE '%мой%' OR LOWER(name) LIKE '%my%';
+
+--Задание3
+
+SELECT g.name AS жанр, COUNT(a.id) AS количество_исполнителей
+FROM genres g
+LEFT JOIN artist_genre ag ON g.id = ag.genre_id
+LEFT JOIN artists a ON ag.artist_id = a.id
+GROUP BY g.name;
+
+
+SELECT COUNT(t.id) AS количество_треков
+FROM tracks t
+JOIN albums al ON t.album_id = al.id
+WHERE al.year IN (2019, 2020);
+
+
+SELECT al.name AS альбом, AVG(t.length) AS средняя_продолжительность
+FROM tracks t
+JOIN albums al ON t.album_id = al.id
+GROUP BY al.name;
+
+
+SELECT DISTINCT a.name AS исполнитель
+FROM artists a
+WHERE a.id NOT IN (
+    SELECT aa.artist_id
+    FROM artist_album aa
+    JOIN albums alb ON aa.album_id = alb.id
+    WHERE alb.year = 2020
+);
+
+
+SELECT c.name AS сборник
+FROM collections c
+JOIN collection_track ct ON c.id = ct.collection_id
+JOIN tracks tr ON ct.track_id = tr.id
+JOIN artist_album aa ON tr.album_id = aa.album_id
+JOIN artists a ON aa.artist_id = a.id
+WHERE a.name = 'Артист 4';
