6bf2a87e3053ed21cb6489c75a879c580f67f1e6
Valeriy <coupe80@gmail.com>
2026-03-20 09:41:05 +0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,47 +1,47 @@
-CREATE TABLE artist (
+CREATE TABLE artists (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL
 );
 
-CREATE TABLE genre (
+CREATE TABLE genres (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE album (
+CREATE TABLE albums (
     id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    release_year INTEGER NOT NULL CHECK(release_year > 0)
+    name TEXT NOT NULL,
+    year INTEGER NOT null
 );
 
-CREATE TABLE track (
+CREATE TABLE tracks (
     id SERIAL PRIMARY KEY,
-    album_id INTEGER REFERENCES album(id),
-    title TEXT NOT NULL,
-    duration INTERVAL NOT NULL,
-    FOREIGN KEY(album_id) REFERENCES album(id)
+    album_id INTEGER REFERENCES albums(id),
+    name TEXT NOT NULL,
+    lenght INTERVAL NOT NULL,
+    FOREIGN KEY(album_id) REFERENCES albums(id)
 );
-
-CREATE TABLE compilation (
+  
+CREATE TABLE collections (
     id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    year INTEGER NOT NULL CHECK(year > 0)
+    name TEXT NOT NULL,
+    year INTEGER NOT NULL 
 );
 
 CREATE TABLE artist_genre (
-    artist_id INTEGER REFERENCES artist(id),
-    genre_id INTEGER REFERENCES genre(id),
+    artist_id INTEGER REFERENCES artists(id),
+    genre_id INTEGER REFERENCES genres(id),
     PRIMARY KEY(artist_id, genre_id)
 );
 
-CREATE TABLE album_artist (
-    album_id INTEGER REFERENCES album(id),
-    artist_id INTEGER REFERENCES artist(id),
+CREATE TABLE artist_album (
+    album_id INTEGER REFERENCES albums(id),
+    artist_id INTEGER REFERENCES artists(id),
     PRIMARY KEY(album_id, artist_id)
 );
 
-CREATE TABLE compilation_track (
-    compilation_id INTEGER REFERENCES compilation(id),
-    track_id INTEGER REFERENCES track(id),
-    PRIMARY KEY(compilation_id, track_id)
+CREATE TABLE collection_track (
+    collection_id INTEGER REFERENCES collections(id),
+    track_id INTEGER REFERENCES tracks(id),
+    PRIMARY KEY(collection_id, track_id)
 );
